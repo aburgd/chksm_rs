8355057e6153f14abad2cbb7b9728dfa94aa4738
Alec Burgdorf <aeburgd@gmail.com>
2019-09-25 14:10:06 -0500

    borrow path for open_file

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod tests {
     #[test]
     fn test_digest_file() {
         let test_filename: String = String::from("test-file.md");
-        let test_file: Vec<u8> = open_file(test_filename);
+        let test_file: Vec<u8> = open_file(&test_filename);
         let test_digest: String = digest_file(String::from("sha256"), test_file);
         let actual_digest: String = String::from("E2318D75A3D991E61F47CBD420368816AA25D0BB186785F522F4DD828F6ACA4A");
         assert_eq!(test_digest.to_uppercase(), actual_digest);
@@ -35,14 +35,15 @@ fn main() {
         .unwrap();
     let checksum: String = args.checksum;
 
-    let file_bytes = open_file(path);
+    let file_bytes = open_file(&path);
     let file_hash = digest_file(checksum, file_bytes);
     let new_now: Instant = Instant::now();
+    println!("{}", &path);
     println!("{}", file_hash);
     println!("time elapsed: {}ms", new_now.duration_since(now).as_millis());
 }
 
-fn open_file(path: String) -> Vec<u8> {
+fn open_file(path: &String) -> Vec<u8> {
     let mut data: Vec<u8> = Vec::new();
     let file = File::open(path).expect("unable to open file");
     let mut buffer = BufReader::new(file);
