8fd7c77240f8ba320c521cfce82064313d47c515
Alec Burgdorf <aeburgd@gmail.com>
2019-10-08 15:56:42 -0500

    format for readability

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use structopt::StructOpt;
+use sha2::{Digest, Sha256, Sha512};
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::PathBuf;
 use std::time::Instant;
-use std::io::{Read, BufReader};
-use std::fs::File;
-use sha2::{Sha256, Sha512, Digest};
+use structopt::StructOpt;
 
 #[cfg(test)]
 mod tests {
@@ -14,7 +14,8 @@ mod tests {
         let test_filename: String = String::from("test-file.md");
         let test_file: Vec<u8> = open_file(&test_filename);
         let test_digest: String = digest_file(String::from("sha256"), test_file);
-        let actual_digest: String = String::from("E2318D75A3D991E61F47CBD420368816AA25D0BB186785F522F4DD828F6ACA4A");
+        let actual_digest: String =
+            String::from("E2318D75A3D991E61F47CBD420368816AA25D0BB186785F522F4DD828F6ACA4A");
         assert_eq!(test_digest.to_uppercase(), actual_digest);
     }
 }
@@ -29,10 +30,7 @@ struct Cli {
 fn main() {
     let now: Instant = Instant::now();
     let args = Cli::from_args();
-    let path = args.path
-        .into_os_string()
-        .into_string()
-        .unwrap();
+    let path = args.path.into_os_string().into_string().unwrap();
     let checksum: String = args.checksum;
 
     let file_bytes = open_file(&path);
@@ -40,7 +38,10 @@ fn main() {
     let new_now: Instant = Instant::now();
     println!("{}", &path);
     println!("{}", file_hash);
-    println!("time elapsed: {}ms", new_now.duration_since(now).as_millis());
+    println!(
+        "time elapsed: {}ms",
+        new_now.duration_since(now).as_millis()
+    );
 }
 
 fn open_file(path: &String) -> Vec<u8> {
@@ -53,20 +54,20 @@ fn open_file(path: &String) -> Vec<u8> {
 
 fn digest_file(digest: String, bytes: Vec<u8>) -> String {
     match digest.as_ref() {
-       "sha256" => {
+        "sha256" => {
             let mut hasher = Sha256::new();
             hasher.input(bytes);
             let result = hasher.result();
             let result_string = format!("{:x}", result);
             return result_string;
-        },
+        }
         "sha512" => {
             let mut hasher = Sha512::new();
             hasher.input(bytes);
             let result = hasher.result();
             let result_string = format!("{:x}", result);
             return result_string;
-        },
+        }
         _ => panic!("no algorithm supplied, aborting"),
     }
 }
